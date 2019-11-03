6ba8f28745d36f43c8f2019210ea35430377f425
JKR96 <jakub.radziwinski@gmail.com>
2019-11-03 22:12:53 +0100

    * Program.cs: * appsettings.json: * QuestionnaireApp.csproj: * 20191103204944_IntitialMigration.cs: * ApplicationDbContextModelSnapshot.cs: * 20191103204944_IntitialMigration.Designer.cs:
    * launchSettings.json: Add connection to database.

---
+++ Data/Migrations/20191103204944_IntitialMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuestionnaireApp.Data.Migrations
+{
+    public partial class IntitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -14,6 +15,7 @@ namespace QuestionnaireApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
