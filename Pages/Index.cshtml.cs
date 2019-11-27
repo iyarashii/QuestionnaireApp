b2d7994cf1640b04176b7ed23e19ec4ddd279dbe
Iyarashii <strider1337v@gmail.com>
2019-11-27 16:36:31 +0100

    Remove template text from login and register pages, modify home page
    Add redirection to Questionnaires page if user is signed in
    and to login page if not.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuestionnaireApp.Models;
 
 namespace QuestionnaireApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly SignInManager<User> _signInManager;
         public bool IsAdmin =>
     HttpContext.User.HasClaim("IsAdmin", bool.TrueString);
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToPage("./Questionnaires/Index");
+            }
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
         }
     }
 }
